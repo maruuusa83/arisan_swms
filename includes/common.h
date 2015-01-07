1a688bac595b93c62fb1654f91034f6a47a8c8e9
Daichi Teruya <tmarusa83@gmail.com>
2015-01-07 01:50:01 +0000

    common.h was added

---
+++ includes/common.h
@@ -0,0 +1,33 @@
+/********************************************************************************
+ * Copyright (C) 2014 Daichi Teruya (@maruuusa83)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License.
+ *
+ * This program is destributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *******************************************************************************/
+#ifndef ___COMMON_H___
+#define ___COMMON_H___
+
+namespace marusa {
+namespace swms {
+
+
+typedef unsigned int JOB_ID;
+typedef unsigned int TASK_ID;
+typedef unsigned int WORKER_ID;
+
+
+} /* swms */
+} /* marusa */
+
+#endif /* ___COMMON_H___ */
