0a3aafbd2e4c5d409f19459f5ae0cffd5b00bc91
Daichi Teruya <tmarusa83@gmail.com>
2015-01-13 09:07:50 +0000

    message type was modified

---
+++ includes/MessagePkt.h
@@ -0,0 +1,80 @@
+/********************************************************************************
+ * Copyright (C) 2014 Daichi Teruya (@maruuusa83)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License.
+ *
+ * This program is destributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *******************************************************************************/
+#ifndef ___MESSAGEPKT_H___
+#define ___MESSAGEPKT_H___
+
+#include "./common.h"
+
+namespace marusa {
+namespace swms {
+
+
+void bytecpy(BYTE *to, const BYTE *from, const int &len);
+
+class MessagePkt
+{
+public:
+	MessagePkt();
+	MessagePkt(const int &to);
+	MessagePkt(const unsigned char &msg_type, const BYTE *data, const unsigned int &size_data);
+	MessagePkt(const int &to, const unsigned char &msg_type, const BYTE *data, const unsigned int &size_data);
+
+	int set_to(const int &to);
+	int set_data(const unsigned char &msg_type, const BYTE *data, const unsigned int &size_data);
+
+	int get_to();
+	void get_msg(BYTE **data);
+
+	static void free_msg(BYTE *data);
+
+	/* Constants */
+	static const int SIZE_MSG_TYPE	= 1;
+	static const int SIZE_DATA_SIZE	= 4;
+
+	static const int TO_STIGMAGY = 0;
+
+	/* Message types */
+	static const int MSG_SEND_TASK		= 0;
+	static const int MSG_REQ_TASKRES	= 1;
+
+	static const int MSG_RET_JOBID		= 2;
+	static const int MSG_NOTE_TASKFIN	= 3;
+	static const int MSG_RET_TASKRES	= 4;
+
+	static const int MSG_RET_TASKLIST	= 5;
+	static const int MSG_RET_TASK		= 6;
+	static const int MSG_NOTE_TASKRES	= 7;
+
+	static const int MSG_REQ_TASKLIST	= 8;
+	static const int MSG_REQ_TASK		= 9;
+
+	static const int MSG_TYPE_PLUS_ONE	= 10;
+
+private:
+	int to = TO_STIGMAGY;
+	unsigned char msg_type = MSG_TYPE_PLUS_ONE;
+	unsigned int size_data = 0;
+	BYTE *data;
+};
+
+
+} /* namespace swms */
+} /* namespace marusa */
+
+#endif /* ___MSGPAKCET_H___ */
+
---
+++ swms/cmc/MessagePkt.cpp
@@ -0,0 +1,98 @@
+/********************************************************************************
+ * Copyright (C) 2014 Daichi Teruya (@maruuusa83)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License.
+ *
+ * This program is destributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *******************************************************************************/
+#include "MessagePkt.h"
+#include <stdlib.h>
+
+namespace marusa {
+namespace swms {
+
+
+void bytecpy(BYTE *to, const BYTE *from, unsigned int len)
+{
+	while (len--){
+		*to++ = *from++;
+	}
+}
+
+/*** MessagePkt class ***/
+/* constractor */
+MessagePkt::MessagePkt() {  }
+
+MessagePkt::MessagePkt(const int &to)
+{
+	set_to(to);
+}
+
+MessagePkt::MessagePkt(const unsigned char &msg_type, const BYTE *data, const unsigned int &size_data)
+{
+	set_data(msg_type, data, size_data);
+}
+
+MessagePkt::MessagePkt(const int &to, const unsigned char &msg_type, const BYTE *data, const unsigned int &size_data)
+{
+	set_to(to);
+	set_data(msg_type, data, size_data);
+}
+
+/* methods */
+int MessagePkt::set_to(const int &to)
+{
+	this->to = to;
+
+	return (0);
+}
+
+int MessagePkt::set_data(const unsigned char &msg_type, const BYTE *data, const unsigned int &size_data)
+{
+	this->msg_type = msg_type;
+
+	bytecpy(this->data, data, size_data);
+	this->size_data = size_data;
+
+	return (0);
+}
+
+int MessagePkt::get_to()
+{
+	return (this->to);
+}
+
+/**
+ * Generate message as byte
+ * This metod generate message data as byte.
+ * BYTE ** : To save massage data
+ * unsigned int * : To save length of message
+ */
+void MessagePkt::get_msg(BYTE **data)
+{
+	*data = (BYTE *)malloc(MessagePkt::SIZE_MSG_TYPE + MessagePkt::SIZE_DATA_SIZE + sizeof(BYTE) * this->size_data);
+
+	*((unsigned char *)*data) = (unsigned char)this->msg_type;
+	*((unsigned int *)(*data + MessagePkt::SIZE_MSG_TYPE)) = (unsigned int)this->size_data;
+	bytecpy(*data + MessagePkt::SIZE_MSG_TYPE + MessagePkt::SIZE_DATA_SIZE, this->data, this->size_data);
+}
+
+void MessagePkt::free_msg(BYTE *data)
+{
+	free(data);
+}
+
+
+} /* namespace grad_res */
+} /* namespace marusa */
+
